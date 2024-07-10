72e3be236e10b9ddcadfb3b7ba0cfaa4007437fd
a.gazzaev <a.gazzaev@mts.ai>
2024-07-10 09:06:27 +0300

    add embedding

--- Phi-3-V/builder.py
+++ Phi-3-V/builder.py
@@ -152,7 +152,7 @@ def load_pretrained_model(model_path, model_base, model_name, load_8bit=False, l
             tokenizer.add_tokens([DEFAULT_IMAGE_PATCH_TOKEN], special_tokens=True)
         if mm_use_im_start_end:
             tokenizer.add_tokens([DEFAULT_IM_START_TOKEN, DEFAULT_IM_END_TOKEN], special_tokens=True)
-        model.resize_token_embeddings(len(tokenizer))
+        model.resize_token_embeddings(32064)
 
         vision_tower = model.get_vision_tower()
         if not vision_tower.is_loaded:
